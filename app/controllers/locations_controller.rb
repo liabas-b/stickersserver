c7d4388b146555b356e56294dfd1e3bd1cb96748
Benoit Liabastre <b.liabastre@gmail.com>
2013-10-25 17:25:54 +0200

    added test header to locations#index.json

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -17,7 +17,7 @@ class LocationsController < ApplicationController
         if (params[:last] == 'true')
           render :json => Location.search(params[:search], "sticker_code").last
         else
-          render json: @locations
+          render json: @locations, token: 'azerty'
         end
       }
       format.js
